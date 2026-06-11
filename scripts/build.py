fae4e744197a6798dbaa8e56c6b029460428a3d8
luca-thompson <lucathompson006@gmail.com>
2026-06-11 16:40:56 +1000

    various changes

--- scripts/build.py
+++ scripts/build.py
@@ -6,9 +6,8 @@ import markdown
 inputDir = "../src/posts"
 outputDir = "../dist"
 assetsDir = "../src/assets"
-postTemplateDir = "../src/post.html"
+postTemplateDir = "../src/templates/post.html"
 jsonPostDataDir = "../dist/posts.json"
-
 
 with open(postTemplateDir) as f:
     postTemplate = f.read()
@@ -17,35 +16,27 @@ posts = []
 
 if os.path.exists(outputDir):
     shutil.rmtree(outputDir)
-
-os.mkdir(outputDir)
+os.makedirs(f"{outputDir}/posts", exist_ok=True)
 
 shutil.copytree(assetsDir, f"{outputDir}/assets", dirs_exist_ok=True)
-
+shutil.copy("../src/templates/home.html", f"{outputDir}/index.html")
 
 for fileName in os.listdir(inputDir):
-    with open(f"./{inputDir}/{fileName}", "r") as f:
+    with open(f"{inputDir}/{fileName}", "r") as f:
         title = f.readline().removeprefix("# ")
-
-    slug = fileName.removesuffix(".md") 
-    url = f"/posts/{slug}/"
-
+    slug = fileName.removesuffix(".md")
+    url = f"/posts/{slug}"
     posts.append({"title": title.strip(), "slug": slug, "url": url})
 
-
 for post in posts:
-
-    with open(f"{inputDir}/{post["slug"]}.md") as content:
-        lines = content.read().splitlines()
-
+    with open(f"{inputDir}/{post['slug']}.md") as f:
+        lines = f.read().splitlines()
     body = "\n".join(lines[1:])
-    html_content = markdown.markdown(body) 
-
+    html_content = markdown.markdown(body)
     output = postTemplate.replace("{{content}}", html_content)
     output = output.replace("{{title}}", post["title"])
     output = output.replace("{{slug}}", post["slug"])
-
-    with open(outputDir + f"/{post["slug"]}.html", 'w') as f:
+    with open(f"{outputDir}/posts/{post['slug']}.html", 'w') as f:
         f.write(output)
 
 with open(jsonPostDataDir, "w") as f:
