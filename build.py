5a0237d263718803042dc588042162aa5c8710f6
luca-thompson <lucathompson006@gmail.com>
2026-07-15 21:44:38 +1000

    fixes to build scipt

--- build.py
+++ build.py
@@ -29,18 +29,18 @@ for fileName in os.listdir(inputDir):
     posts.append({"title": title.strip(), "slug": slug, "url": url})
 
 for i in range(len(posts)):
-    with open(f"{inputDir}/{post[i]['slug']}.md") as f:
+    with open(f"{inputDir}/{posts[i]['slug']}.md") as f:
         lines = f.read().splitlines()
     body = "\n".join(lines[1:])
     html_content = markdown.markdown(body)
     output = postTemplate.replace("{{content}}", html_content)
-    output = output.replace("{{title}}", post[i]["title"])
-    output = output.replace("{{slug}}", post[i]["slug"])
+    output = output.replace("{{title}}", posts[i]["title"])
+    output = output.replace("{{slug}}", posts[i]["slug"])
 
-    output = output.replace("{{leftHref}}", post[(i-1) % len(posts)]["url"])
-    output = output.replace("{{rightHref}}", post[(i+1) % len(posts)]["url"])
+    output = output.replace("{{leftHref}}", posts[(i-1) % len(posts)]["url"])
+    output = output.replace("{{rightHref}}", posts[(i+1) % len(posts)]["url"])
 
-    with open(f"{outputDir}/posts/{post[i]['slug']}.html", 'w') as f:
+    with open(f"{outputDir}/posts/{posts[i]['slug']}.html", 'w') as f:
         f.write(output)
 
 with open(jsonPostDataDir, "w") as f:
